2bca76a8880dd2614a3a6fdb8cccae9b7cab281c
Bharatsharma77 <bharatsharma9368@gmail.com>
2021-07-24 15:01:43 +0530

    add layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import User from "./components/user";
+import { Route } from "react-router-dom";
+
+// HOC
+import DefaultHOC from "./HOC/Default.HOC";
+import MovieHOC from "./HOC/Movie.HOC";
+
+// Pages
+import HomePage from "./pages/Home.page";
+import Movie from "./pages/Movie.page";
+
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
-    <div>
-      <h1>Users</h1>
-      <User name="Pavan" description="FSWD" />
-    </div>
+    <>
+      <DefaultHOC path="/" exact component={HomePage} />
+      <MovieHOC path="/movie/:id" exact component={Movie} />
+    </>
   );
 }
 
 export default App;
-
-// Props - A data that is being passed from one component to another.
-// state - A set of data that an individual component holds.
